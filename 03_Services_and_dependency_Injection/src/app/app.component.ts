a4bb56ee575dd82b2768a84beef4c0311ddb4c48
Amlan <das193895@gmail.com>
2024-06-02 01:17:00 +0530

    services

---
+++ 03_Services_and_dependency_Injection/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Services_and_dependency_Injection';
+}
--- 03_Services_and_dependency_Injection/src/app/app.module.ts
+++ 03_Services_and_dependency_Injection/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { TopHeaderComponent } from './top-header/top-header.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { MainMenuComponent } from './header/main-menu/main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    TopHeaderComponent,
-    ProductListComponent,
-    MainMenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
